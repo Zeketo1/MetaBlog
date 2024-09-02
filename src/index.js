091fd406044b7a1ada3603488985b2aa716a534f
Zeketo <ikorofrancis24@gmail.com>
2024-09-02 13:04:01 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Logo2 from "./assets/Images/Homepage/Logo2.png";
 import LogoS from "./assets/Images/Homepage/LogoS.svg";
 import LogoS2 from "./assets/Images/Homepage/LogoS2.svg";
 // About us
-import Hands from "./assets/Images/Aboutus/Hands.jpg";
+import Hands from "./assets/Images/Aboutus/hands.jpg";
 import Teamwork from "./assets/Images/Aboutus/teamwork.jpg";
 
 // console.log(blogPosts);
